60a37622470647dd74b5d50ef897103542c18a96
Reahana <reahana233@gmail.com>
2024-01-08 22:07:31 +0600

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" />
+            <a href="1">1</a><a href="2">2</a><a href="3">3</a><a href="4">4</a>
+        </div>
+    );
+};
+
+export default Header;

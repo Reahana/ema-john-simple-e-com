7aed71a234f4eee43556b87c14f5971d127fc0f9
Reahana <reahana233@gmail.com>
2024-01-08 23:36:00 +0600

    shop

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts]= useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
     return (
-        <div>
-            
+        <div className='shop-container'>
+            <div className="products-container">
+                <h2>Products coming soon : {products.length}</h2>
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+            </div>
         </div>
     );
 };

35d6bbb04370729631ca24cf0d59a1fd9be46cd8
Reahana <reahana233@gmail.com>
2024-01-09 10:53:22 +0600

    Product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './Product.css'
 
-const Product = () => {
+const Product = (props) => {
+const{img, name, seller, quantity,price}= props.product;
     return (
-        <div>
-            
+        <div className='product'>
+            <img src={img} alt="" />
+            <h6>{name}</h6>
         </div>
     );
 };

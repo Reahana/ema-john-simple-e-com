7372d4c7e9f3a8ed03f90d58f339cb6360ac21ab
Reahana <reahana233@gmail.com>
2024-01-15 22:25:11 +0600

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>&copy; Ema & John || 2024</p>
+        </div>
+    );
+};
+
+export default Footer;

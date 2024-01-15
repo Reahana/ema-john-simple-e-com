ab84b3a49d13a1bb1f9d3263d31d1e2c4da552f8
Reahana <reahana233@gmail.com>
2024-01-15 20:47:55 +0600

    cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,9 +8,12 @@ const Cart = (props) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping =totalShipping+ product.shipping
+        // product.quantity =product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping =totalShipping+ product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7/100;
@@ -19,7 +22,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-                <p>Selected item: {cart.length}</p>
+                <p>Selected item: {quantity}</p>
                 <p>Total Price: $ {totalPrice}</p>
                 <p>Shipping: $ {totalShipping}</p>
                 <p>Tax: $ {tax.toFixed(2)}</p>

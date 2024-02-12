736c9edb4b0af18b84fbd8fdd8100b9539598d88
Reahana <reahana233@gmail.com>
2024-02-12 23:37:33 +0600

    cart

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,7 +1,29 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader =  async () => {
  const loadedProducts = await fetch ('products.json');
  const products = await loadedProducts.json();
- console.log(products);
+ 
+
+ const storedCart = getShoppingCart();
+ const savedCart = [];
+ for (const id in storedCart){
+  
+const addedProduct = products.find(pd => pd.id === id)
+if(addedProduct){
+    const quantity = storedCart[id];
+    addedProduct.quantity = quantity;
+    savedCart.push(addedProduct);
+    }
+
+}
+return savedCart;
+//step 5
+// setCart(savedCart);
+// },[products])
+
+
+
 }
 
 export default cartProductsLoader;

b7834796858f5b7745de9b47160bbcf4f8175878
Reahana <reahana233@gmail.com>
2024-02-12 21:12:25 +0600

    nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './Nav.css'
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <div className='nav'>
-            <a href="/">Shop</a>
-            <a href="/order">Orders</a>
-            <a href="/inventory">Inventory</a>
-            <a href="/login">Login</a> 
+            <Link to="/">Shop</Link>
+            <Link to="/orders">Orders</Link>
+            <Link to="/inventory">Inventory</Link>
+            <Link to="/login">Login</Link> 
         </div>
     );
 };

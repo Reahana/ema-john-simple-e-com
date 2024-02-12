26adf9f380ea3f83a464fbb85b73fd6205f28964
Reahana <reahana233@gmail.com>
2024-02-12 20:48:09 +0600

    nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ import './Nav.css'
 const Nav = () => {
     return (
         <div className='nav'>
-            <a href="/shop">Shop</a>
+            <a href="/">Shop</a>
             <a href="/order">Orders</a>
             <a href="/inventory">Inventory</a>
             <a href="/login">Login</a> 

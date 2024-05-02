45d16a2022cd5346e7131acee70f4b6173eb7b04
Reahana <reahana233@gmail.com>
2024-05-02 19:33:10 +0600

    route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
+  import {
+createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
